fe198e5d11a46c15363f45bdec4a3f7114c6502d
Bushra Shireen <sbushra109@gmail.com>
2021-07-26 23:08:51 +0530

    message

--- app.js
+++ app.js
@@ -100,6 +100,12 @@ Post.findOne({_id: requestedPostId}, function(err, post){
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == NULL || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function() {
+  console.log("Server has started successfully.");
 });
